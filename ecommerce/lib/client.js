c19aa73c37b1a5e1d4829a0aeb62f71a24bdfa67
Nienge David <97910242+NiengeDavid@users.noreply.github.com>
2024-07-23 12:08:37 +0100

    Deploy bugs

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -8,7 +8,7 @@ export const client = createClient({
   dataset: "production",
   apiVersion: "2024-04-16",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: process.env.NEXT_APP_SANITY_TOKEN,
   ignoreBrowserTokenWarning: true
 });
 
